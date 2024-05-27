737b9ae8dd63a0a08672f0e9db009d58c7daec5e
janvanderwatt <janvanderwatt@gmail.com>
2024-05-27 13:55:44 +1000

    Separated types. Added GRB_300 strip.

--- include/LedConfigurations.h
+++ include/LedConfigurations.h
@@ -5,7 +5,10 @@
 #include "Configuration.h"
 #include "DisplayModes.h"
 
+#include "Types.h"
+
 #include "strips/party55.h"
 #include "strips/GRB_144.h"
+#include "strips/GRB_300.h"
 #include "strips/nel.h"
 #include "strips/stl.h"
