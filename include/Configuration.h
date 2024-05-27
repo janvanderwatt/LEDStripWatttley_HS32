e3c2c57e36a3c8381d6a669de4750987021ceb58
janvanderwatt <janvanderwatt@gmail.com>
2024-05-27 13:54:48 +1000

    Don't do defines if configured in environment.

--- include/Configuration.h
+++ include/Configuration.h
@@ -4,23 +4,26 @@
 
 #include "Constants.h"
 
+#if !defined(DESTINATION_AND_STRIP)
 // ---- defines for using [4WF Wifi] and strips
 #define DESTINATION_WATTLEY
 // #define STRIP_4WF_MASTER_BATHROOM
-// #define STRIP_4WF_OFFICE
+#define STRIP_4WF_OFFICE
 // #define STRIP_4WF_DINING_ROOM
 // #define STRIP_4WF_STAIRCASE
 // #define STRIP_WARM_WHITE_TEST
 // ---- defines for using [Nel Wifi] and strips
 // #define DESTINATION_NEL
-#define STRIP_72WHITE_MORNE_ROOM
-// ---- defines for using [STL Wifi] and strips
-// #define DESTINATION_STL
-// #define STL_COMMON_CONFIGURATION
-// #define STL_KITCHEN_STRIP
-// #define STL_LOUNGE_STRIP
-// #define STL_BEDROOM_STRIP
-// ---- defines for using [Maraetia Wifi] and strips
-// #define DESTINATION_MARAETAI
-// ---- defines for using [MHS Wifi] and strips
-// #define DESTINATION_MHS
+// #define STRIP_72WHITE_MORNE_ROOM
+// #define MORNE_ROOM_TEST
+//  ---- defines for using [STL Wifi] and strips
+//  #define DESTINATION_STL
+//  #define STL_COMMON_CONFIGURATION
+//  #define STL_KITCHEN_STRIP
+//  #define STL_LOUNGE_STRIP
+//  #define STL_BEDROOM_STRIP
+//  ---- defines for using [Maraetia Wifi] and strips
+//  #define DESTINATION_MARAETAI
+//  ---- defines for using [MHS Wifi] and strips
+//  #define DESTINATION_MHS
+#endif  // !defined(DESTINATION_AND_STRIP)
